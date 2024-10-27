a4c1552a71e90ae26833e29d135bb4fa15c71e1e
Thế Vĩ <119785056+chauthevi2004@users.noreply.github.com>
2024-10-27 08:47:03 +0700

    Update attendance_app.py

--- attendance_app.py
+++ attendance_app.py
@@ -12,7 +12,7 @@ def connect_to_google_sheets_by_id(sheet_id):
     client = gspread.authorize(creds)
 
     try:
-        sheet = client.open_by_key(sheet_id).worksheet("Bac-1")
+        sheet = client.open_by_key(sheet_id).worksheet(sheet_name)
         return sheet
     except gspread.SpreadsheetNotFound:
         st.error("Không tìm thấy Google Sheet. Kiểm tra ID hoặc quyền chia sẻ.")
